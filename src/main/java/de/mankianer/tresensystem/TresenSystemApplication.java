0a39a112dc6d1fb04805f794db11feed0e9b8fa1
Mankianer <Marvin.woelk@web.de>
2022-01-04 19:17:11 +0100

    init

---
+++ src/main/java/de/mankianer/tresensystem/TresenSystemApplication.java
@@ -0,0 +1,13 @@
+package de.mankianer.tresensystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TresenSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TresenSystemApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/mankianer/tresensystem/TresenSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mankianer.tresensystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresenSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
